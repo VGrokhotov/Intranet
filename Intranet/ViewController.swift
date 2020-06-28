e065e5fa04b589230793b0b5069013a0b7c68ca5
Vladislav <vlad@grokhotov.ru>
2020-06-28 16:38:31 +0300

    Initial Commit

---
+++ Intranet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intranet
+//
+//  Created by Владислав on 28.06.2020.
+//  Copyright © 2020 Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
