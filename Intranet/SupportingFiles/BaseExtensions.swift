c5c9d6a4c1a20d5a2a573650557e1751441fc0fb
Vladislav <vlad@grokhotov.ru>
2020-06-28 17:31:05 +0300

    changed structure of files consistence

---
+++ Intranet/SupportingFiles/BaseExtensions.swift
@@ -0,0 +1,47 @@
+//
+//  BaseExtensions.swift
+//  Intranet
+//
+//  Created by Владислав on 28.06.2020.
+//  Copyright © 2020 Vladislav. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func disable(views: UIControl...) {
+        for view in views {
+            view.isEnabled = false
+        }
+    }
+    
+    func activate(views: UIControl...) {
+        for view in views {
+            view.isEnabled = true
+        }
+    }
+    
+    func hide(view: UIView) {
+        UIView.animate(withDuration: 0.5, animations: {
+            view.alpha = 0
+        }) { (finished) in
+            view.isHidden = finished
+        }
+    }
+    
+    func show(view: UIView) {
+        view.alpha = 0
+        view.isHidden = false
+        UIView.animate(withDuration: 0.6) {
+            view.alpha = 1
+        }
+    }
+    
+    func configurate(button: UIButton) {
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 2.0
+        button.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        button.clipsToBounds = true
+    }
+}
